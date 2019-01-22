26fc921d4f02ae3e4be19125fbe3b7b5b6f5d5e8
Thomas Theunen <thomas.theunen@gmail.com>
2019-01-22 19:55:32 +0100

    Added prevention for double presses and added support for continuous

--- arduino_ir_lamp.ino
+++ arduino_ir_lamp.ino
@@ -18,6 +18,7 @@ CRGB endclr;
 CRGB midclr;
 
 boolean onoff;
+unsigned long lastCommand = millis();
 
 //set up IR receiver information
 IRrecv irrecv(RECV_PIN);
@@ -34,7 +35,7 @@ void setup()
   irrecv.enableIRIn();
 
   //set up LED strip information
-  FastLED.addLeds<LED_TYPE, LEDPIN>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, LEDPIN>(leds, NUM_LEDS).setCorrection(UncorrectedColor );
   FastLED.setBrightness(BRIGHTNESS);
   fill_solid(leds, NUM_LEDS, CRGB::Black);
   FastLED.show();
@@ -45,136 +46,141 @@ void setup()
 //<—————————————————————————LOOP———————————————————————————————>
 void loop() {
   if (irrecv.decode(&results)) {
-    FastLED.delay(1000 / 120);
-    long int result = results.value;
+    if (( millis() - lastCommand) > 350) {
+      lastCommand = millis();
+      FastLED.delay(1000 / 120);
+      long int result = results.value;
 
-    if (results.value == rep) {
-      result = lastvalue;
-    }
-
-    lastvalue = result;
-
-    Serial.println(result);
-
-    //<——————————————————BUTTON FUNCTIONS—————————————————————————>
-    //power button = plain white
-    if (result == 16761405) {
-      onoff = !onoff;
-      if (onoff) {
-        fill_solid(leds, NUM_LEDS, CRGB::AliceBlue);
-      } else {
-        fill_solid(leds, NUM_LEDS, CRGB::Black);
+      if (results.value == rep) {
+        result = lastvalue;
       }
 
-      FastLED.show();
-    }
+      lastvalue = result;
 
-    //vol+ = brightness up
-    if (result == 16754775) {
-      if (BRIGHTNESS < 250) {
-        BRIGHTNESS += 10;
+      Serial.println(result);
+
+      //<——————————————————BUTTON FUNCTIONS—————————————————————————>
+      //power button = plain white
+      if (result == 16761405) {
+        onoff = !onoff;
+        if (onoff) {
+          fill_solid(leds, NUM_LEDS, CRGB::AliceBlue);
+        } else {
+          fill_solid(leds, NUM_LEDS, CRGB::Black);
+        }
+
+        FastLED.show();
       }
-      FastLED.setBrightness(BRIGHTNESS);
-      FastLED.show();
-    }
 
-    //vol- = brightness down
-    if (result == 16769055) {
-      if (BRIGHTNESS > 10) {
-        BRIGHTNESS -= 10;
+      //vol+ = brightness up
+      if (result == 16754775) {
+        if (BRIGHTNESS < 250) {
+          BRIGHTNESS += 10;
+        }
+        FastLED.setBrightness(BRIGHTNESS);
+        FastLED.show();
       }
-      FastLED.setBrightness(BRIGHTNESS);
-      FastLED.show();
+
+      //vol- = brightness down
+      if (result == 16769055) {
+        if (BRIGHTNESS > 10) {
+          BRIGHTNESS -= 10;
+        }
+        FastLED.setBrightness(BRIGHTNESS);
+        FastLED.show();
+      }
+
+      //play/pause = cycles through colours
+      if (result == 16736925) {
+        uint8_t i = beatsin8(130, 0, 150);
+        fill_solid(leds, NUM_LEDS, CHSV(i, 255, 150));
+        FastLED.show();
+      }
+
+      //left = Custom gradient blend
+      if (result == 0xFD20DF) {
+        uint8_t speed = beatsin8(10, 0, 255);
+        endclr = blend(CHSV(160, 185, 255), CRGB::Pink, speed);
+        midclr = blend(CRGB::Purple, CRGB::Green, speed);
+
+        fill_gradient_RGB(leds, 0, endclr, NUM_LEDS / 2, midclr);
+        fill_gradient_RGB(leds, NUM_LEDS / 2 + 1, midclr, NUM_LEDS, endclr);
+        FastLED.show();
+      }
+
+
+      if (result == 16769565) {
+        uint8_t i = beatsin8(130, 0, 150);
+        uint8_t j = beatsin8(180, 0, 150);
+        fill_gradient (leds, NUM_LEDS,  CHSV(i, 255, 150), CHSV(j, 255, 150), SHORTEST_HUES);
+        FastLED.show();
+      }
+
+
+      if (result == 16753245) {
+
+        fill_gradient (leds, NUM_LEDS,  CHSV(beatsin8(180, 0, 150), 255, 150), CHSV( beatsin8(130, 0, 150), 255, 150), LONGEST_HUES);
+        FastLED.show();
+      }
+
+      if (result == 16738455) {
+        fill_solid(leds, NUM_LEDS, CRGB::White);
+        FastLED.show();
+      }
+
+      if (result == 16724175) {
+        fill_solid(leds, NUM_LEDS, CRGB::Blue);
+        FastLED.show();
+      }
+
+      // 2 = sunset (orange & blue)
+      if (result == 16718055) {
+        fill_solid(leds, NUM_LEDS, CRGB::Green);
+        FastLED.setBrightness(BRIGHTNESS);
+        FastLED.show();
+      }
+
+      if (result == 16743045) {
+        fill_solid(leds, NUM_LEDS, CRGB::Red);
+        FastLED.show();
+      }
+
+      if (result == 16716015) {
+        fill_solid(leds, NUM_LEDS, CRGB::Pink);
+        FastLED.setBrightness(BRIGHTNESS);
+        FastLED.show();
+      }
+
+      if (result == 16726215) {
+        fill_solid(leds, NUM_LEDS, CRGB::Purple);
+        FastLED.show();
+      }
+
+      // 6 = rain
+      if (result == 16734885) {
+        fill_solid(leds, NUM_LEDS, CRGB::LightBlue);
+      }
+
+      // 7 = acid rain
+      if (result == 851901943) {
+
+      }
+
+      // 8 = snow
+      if (result == 16730805) {
+
+      }
+
+      // 9 = storm
+      if (result == 16732845) {
+
+      }
+
+      irrecv.resume(); // Receive the next value
     }
-
-    //play/pause = cycles through colours
-    if (result == 16736925) {
-      uint8_t i = beatsin8(130, 0, 150);
-      fill_solid(leds, NUM_LEDS, CHSV(i, 255, 150));
-      FastLED.show();
-    }
-
-    //left = Custom gradient blend
-    if (result == 0xFD20DF) {
-      uint8_t speed = beatsin8(10, 0, 255);
-      endclr = blend(CHSV(160, 185, 255), CRGB::Pink, speed);
-      midclr = blend(CRGB::Purple, CRGB::Green, speed);
-
-      fill_gradient_RGB(leds, 0, endclr, NUM_LEDS / 2, midclr);
-      fill_gradient_RGB(leds, NUM_LEDS / 2 + 1, midclr, NUM_LEDS, endclr);
-      FastLED.show();
-    }
-
-
-    if (result == 16769565) {
-      uint8_t i = beatsin8(130, 0, 150);
-      uint8_t j = beatsin8(180, 0, 150);
-      fill_gradient (leds, NUM_LEDS,  CHSV(i, 255, 150), CHSV(j, 255, 150), SHORTEST_HUES);
-      FastLED.show();
-    }
-
-
-    if (result == 16753245) {
-
-      fill_gradient (leds, NUM_LEDS,  CHSV(beatsin8(180, 0, 150), 255, 150), CHSV( beatsin8(130, 0, 150), 255, 150), LONGEST_HUES);
-      FastLED.show();
-    }
-
-    if (result == 16738455) {
-      fill_solid(leds, NUM_LEDS, CRGB::White);
-      FastLED.show();
-    }
-
-    if (result == 16724175) {
-      fill_solid(leds, NUM_LEDS, CRGB::Blue);
-      FastLED.show();
-    }
-
-    // 2 = sunset (orange & blue)
-    if (result == 16718055) {
-      fill_solid(leds, NUM_LEDS, CRGB::Green);
-      FastLED.setBrightness(BRIGHTNESS);
-      FastLED.show();
-    }
-
-    if (result == 16743045) {
-      fill_solid(leds, NUM_LEDS, CRGB::Red);
-      FastLED.show();
-    }
-
-    if (result == 16716015) {
-      fill_solid(leds, NUM_LEDS, CRGB::Pink);
-      FastLED.setBrightness(BRIGHTNESS);
-      FastLED.show();
-    }
-
-    if (result == 16726215) {
-      fill_solid(leds, NUM_LEDS, CRGB::Purple);
-      FastLED.show();
-    }
-
-    // 6 = rain
-    if (result == 16734885) {
-      fill_solid(leds, NUM_LEDS, CRGB::LightBlue);
-    }
-
-    // 7 = acid rain
-    if (result == 851901943) {
-
-    }
-
-    // 8 = snow
-    if (result == 16730805) {
-
-    }
-
-    // 9 = storm
-    if (result == 16732845) {
-
-    }
-
-    irrecv.resume(); // Receive the next value
   }
+
+  delay(75);
 }
 
 //<——————————————————————OTHER FUNCTIONS————————————————————>
